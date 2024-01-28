5f225c6b706301ca6b2697b7a85de3d8a2500121
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-01-28 02:39:17 +0000

    Finish23

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @booknew = Book.new
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -32,15 +35,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
