4cedb696de010f6401eb997811e438f7dc0ac7f6
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-02-02 11:58:32 +0000

    修正Ok

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def show
     @book = Book.find(params[:id])
     @user = @book.user
@@ -22,10 +25,6 @@ class BooksController < ApplicationController
   end
 
   def edit
-    book = Book.find(params[:id])
-    unless book.user.id == current_user.id
-      redirect_to books_path
-    end
     @book = Book.find(params[:id])
   end
 
@@ -48,5 +47,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+      book = Book.find(params[:id])
+      unless book.user == current_user
+        redirect_to books_path
+      end
   end
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,10 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
 
   def search
     @word = params[:word]
-    @range = params[:range]
-    if @range == "User"
+    @model = params[:model]
+    if @model == "User"
       @users = User.looks(params[:search], params[:word])
     else
       @books = Book.looks(params[:search], params[:word])
