346989b091df8e5a523c6c8809f198320c3af617
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-02-05 07:41:07 +0000

    group作成newまで

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,21 @@
+class GroupsController < ApplicationController
+  
+  def create
+  end 
+  
+  def edit
+  end 
+  
+  def update
+  end 
+  
+  def destroy
+  end 
+  
+  def index
+  end 
+  
+  def show
+  end 
+  
+end
