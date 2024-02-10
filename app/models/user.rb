295e949813340e2c585fdd843563ed88c49a99ec
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-02-10 08:14:29 +0000

    9aFinish

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :group_users, dependent: :destroy
   has_many :groups, through: :group_users
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :rooms, through: :entries
+  has_many :view_counts, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create, :show]
   resources :groups do
     resource :group_users, only:[:create, :destroy]
     get "new/mail" => "groups#new_mail"
